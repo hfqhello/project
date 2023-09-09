a5bb983014714343ca6e3a75df8386f1b46edec9
hfq <1610766446@qq.com>
2023-09-09 17:17:20 +0800

    添加套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annation.AutoFill;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +13,14 @@ public interface SetmealDishMapper {
 
     List<Long> getSetmealIdsByDishIds(List<Long> ids);
 
+    /**
+     * 插入新建套餐数据
+     * */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Setmeal setmeal);
+
+    /**
+     *  保存套餐和菜品的关联关系
+    * */
+    void insertBatch(List<SetmealDish> setmealDishes);
 }
