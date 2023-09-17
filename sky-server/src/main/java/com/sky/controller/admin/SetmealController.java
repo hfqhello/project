2daab45aec2ca3681e9e99010992be08b31d2e4e
hfq <1610766446@qq.com>
2023-09-17 16:13:13 +0800

    使用spring-boot-starter-cache 缓存框架将数据缓存道 redis中

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +30,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CachePut(cacheNames ="setmealCache" ,key ="#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         setmealService.saveWithDish(setmealDTO);
         return  Result.success();
@@ -45,6 +48,7 @@ public class SetmealController {
      * */
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames ="setmealCache" ,allEntries = true)
     public  Result delete(@RequestParam List<Long> ids){
         setmealService.deleteBatch(ids);
         return  Result.success();
@@ -64,6 +68,7 @@ public class SetmealController {
      * */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames ="setmealCache" ,allEntries = true)
     public  Result update(@RequestBody  SetmealVO setmealVO ){
         setmealService.update(setmealVO);
         return Result.success();
@@ -74,6 +79,7 @@ public class SetmealController {
      * */
     @PostMapping("/status/{status}")
     @ApiOperation("餐停起售")
+    @CacheEvict(cacheNames ="setmealCache" ,allEntries = true)
     public  Result startOrStop(@PathVariable Integer status ,Long id){
         setmealService.startOrStop(status,id);
         return  Result.success();
