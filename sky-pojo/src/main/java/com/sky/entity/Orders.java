b9c03288e3bd7b3a83f8fa01ae6b4e653db716ab
hfq <1610766446@qq.com>
2023-09-22 21:23:40 +0800

    管理端 订单搜索接口和各个状态订单数量统计接口完成

--- sky-pojo/src/main/java/com/sky/entity/Orders.java
+++ sky-pojo/src/main/java/com/sky/entity/Orders.java
@@ -23,8 +23,8 @@ public class Orders implements Serializable {
      */
     public static final Integer PENDING_PAYMENT = 1;
     public static final Integer TO_BE_CONFIRMED = 2;
-    public static final Integer CONFIRMED = 3;
     public static final Integer DELIVERY_IN_PROGRESS = 4;
+    public static final Integer CONFIRMED = 3;
     public static final Integer COMPLETED = 5;
     public static final Integer CANCELLED = 6;
 
