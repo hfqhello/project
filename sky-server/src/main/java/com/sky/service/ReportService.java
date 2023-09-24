f85472f08d9225497751eebc8f3d72d22382a3fc
hfq <1610766446@qq.com>
2023-09-25 00:06:48 +0800

    用户端统计接口实现

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
@@ -14,6 +15,14 @@ public interface ReportService {
      * @return
      */
     TurnoverReportVO getTurnover(LocalDate begin, LocalDate end);
+
+    /**
+     * 用户数据统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 
 
 }
